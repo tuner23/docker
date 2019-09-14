d9cdd269d6998e2b803cc4fca4fba4b07f5c9fef
tuner <toni@dimtsoudis.org>
2019-09-14 09:06:09 +0200

    docker: update docker scripts for develwq

--- charts/devel/docker/devel/devel.start_container.sh
+++ charts/devel/docker/devel/devel.start_container.sh
@@ -2,18 +2,23 @@
   
 ## init: comment USER ${USER} and WORKDIR ${REPO_PATH} in dockerfiles/devel.df
 if [ `id -u` == 0 ] ; then
-  mkdir -p ${REPO_PATH}
-  cd ${REPO_PATH}
-  if [ `ls -1  ${REPO_PATH} | wc -l` == 0 ] ; then
-    git clone git://git:/data/repo/ .
+  if [ ! -d ${REPO_PATH} ] ; then
+    mkdir -p ${REPO_PATH}
   fi
-  su devel -c 'git config --global user.email "devel-devel@mail.arx"'
-  su devel -c 'git config --global user.name "devel"'
-  chown -R ${USER}:${USER} ${REPO_PATH}
-  /bin/bash
+  if [ ! -d ${REPO_PATH}/.git ] ; then
+    cd ${REPO_PATH}
+    git clone git://git:/data/repo/ .
+    su devel -c 'git config --global user.email "devel-devel@mail.arx"'
+    su devel -c 'git config --global user.name "devel"'
+    chown -R ${USER}:${USER} ${REPO_PATH}
+    /bin/bash
+  else
+    cd ${REPO_PATH}
+    git pull
+    tail -f /dev/null
+  fi
 else
-  cd ${REPO_PATH}
-  git pull
-  tail -f /dev/null
+  echo "Running as root!"
+  /bin/bash
 fi
 
