d87b4abbfe4550eb1ba252b1b0569371f94c5bfe
tuner <toni@dimtsoudis.org>
2019-09-21 06:17:46 +0200

    docker: run git as standard user

--- charts/git/docker/git/start_container.sh
+++ charts/git/docker/git/start_container.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 ## for init: comment WORKDIR ${REPO_PATH} in dockerfiles/django.df
-if [ `id -u` == 0 ] ; then
+if [ ! -d ${REPO_PATH} ] ; then
   mkdir -p ${REPO_PATH}
-  cd ${REPO_PATH}
-  ## create repos
-  if [[ ! -d "${REPO_PATH}/hooks" ]] ; then
-    echo "No repository found at ${REPO_PATH}. Initializing!"
-    /usr/bin/git init --bare
-    chown -R ${USER}:${USER} ${REPO_PATH}
-  fi
+fi
+cd ${REPO_PATH}
+## create repos
+if [[ ! -d "${REPO_PATH}/hooks" ]] ; then
+  echo "No repository found at ${REPO_PATH}. Initializing!"
+  /usr/bin/git init --bare
   
   ## Add post-commit hook
   cat <<\EOF > ${REPO_PATH}/hooks/post-commit
@@ -19,8 +18,8 @@ JENKINS_IP="172.17.0.3"
 exec curl -v http://${JENKINS_IP}:8080/git/notifyCommit?url='git://git:9418/data/git'
 EOF
   chmod 755 ${REPO_PATH}/hooks/post-commit
-  chown git:git ${REPO_PATH}/hooks/post-commit
-  
+  chown -R ${USER}:${USER} ${REPO_PATH}
+fi
 #  ## create ssh env
 #  if [[ ! -d "${REPO_PATH}/.ssh" ]] ; then
 #    mkdir "${REPO_PATH}/.ssh"
@@ -28,8 +27,6 @@ EOF
 #    chown ${USER}:${USER} -R ${REPO_PATH}/.ssh
 #    chmod go-rwx ${REPO_PATH}/.ssh
 #  fi
-  /usr/libexec/git-core/git-daemon --verbose --enable=receive-pack --enable=upload-archive --export-all --user=${USER} --group=${USER}
-else
-  echo "Run as root!"
-  /bin/bash
-fi
+
+## Starting git daemon
+/usr/libexec/git-core/git-daemon --verbose --enable=receive-pack --enable=upload-archive --export-all --user=${USER} --group=${USER}
