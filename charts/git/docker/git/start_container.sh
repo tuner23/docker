23a799ca1d9d5b71b915529860653320d506f92f
tuner <toni@dimtsoudis.org>
2019-11-01 19:50:29 +0100

    docker: trouble-shoot invalid cross-device link

--- charts/git/docker/git/start_container.sh
+++ charts/git/docker/git/start_container.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-if [ `id -u` == 0 ] ; then
-  if [ ! -d ${REPO_PATH} ] ; then
-    groupadd ${USER}
-    useradd -m -d ${REPO_PATH} -g root -s /usr/bin/git-shell ${USER}
-  fi
+if [ ! -d ${REPO_PATH} ] ; then
+  groupadd ${USER}
+  useradd -m -d ${REPO_PATH} -g root -s /usr/bin/git-shell ${USER}
+fi
+
+cd ${REPO_PATH}
+## create repos
+if [[ ! -d "${REPO_PATH}/hooks" ]] ; then
+  echo "No repository found at ${REPO_PATH}. Initializing!"
+  /usr/bin/git init --bare
   
-  cd ${REPO_PATH}
-  ## create repos
-  if [[ ! -d "${REPO_PATH}/hooks" ]] ; then
-    echo "No repository found at ${REPO_PATH}. Initializing!"
-    /usr/bin/git init --bare
-    
-    ## Add post-commit hook
-    cat <<\EOF > ${REPO_PATH}/hooks/post-commit
+  ## Add post-commit hook
+ cat <<\EOF > ${REPO_PATH}/hooks/post-commit
 #!/bin/sh
 exec curl -v http://jenkins:8080/git/notifyCommit?url='git://git:9418/data/repo'
 EOF
-    chmod 755 ${REPO_PATH}/hooks/post-commit
-    chown -R ${USER}:${USER} ${REPO_PATH}
-  fi
-  #  ## create ssh env
-  #  if [[ ! -d "${REPO_PATH}/.ssh" ]] ; then
-  #    mkdir "${REPO_PATH}/.ssh"
-  #    touch ${REPO_PATH}/.ssh/authorized_keys
-  #    chown ${USER}:${USER} -R ${REPO_PATH}/.ssh
-  #    chmod go-rwx ${REPO_PATH}/.ssh
-  #  fi
-  
-  ## Starting git daemon
-  /usr/libexec/git-core/git-daemon --verbose --base-path=/data/repo --enable=receive-pack --enable=upload-archive --export-all --user=${USER} --group=${USER}
-else
-  echo "Run as root!"
-  echo "..or do stuff with sudo"
-  /bin/bash
+  chmod 755 ${REPO_PATH}/hooks/post-commit
 fi
+ 
+chown -R ${USER}:${USER} ${REPO_PATH}
+
+#  ## create ssh env
+#  if [[ ! -d "${REPO_PATH}/.ssh" ]] ; then
+#    mkdir "${REPO_PATH}/.ssh"
+#    touch ${REPO_PATH}/.ssh/authorized_keys
+#    chown ${USER}:${USER} -R ${REPO_PATH}/.ssh
+#    chmod go-rwx ${REPO_PATH}/.ssh
+#  fi
+  
+## Starting git daemon
+/usr/libexec/git-core/git-daemon --verbose --base-path=/data/repo --enable=receive-pack --enable=upload-archive --export-all --user=${USER} --group=${USER}
 
 while true; do sleep 30; done;
