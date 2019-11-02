32809896af2dfba649b3eb53a1bd96328634127d
tuner <toni@dimtsoudis.org>
2019-11-02 15:47:03 +0100

    docker: trouble-shoot invalid cross-device link

--- charts/devel/docker/devel/start_container.sh
+++ charts/devel/docker/devel/start_container.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
   
-if [ `id -u` != 0 ] ; then
-  if [ ! -d ${REPO_PATH} ] ; then
-    mkdir -p ${REPO_PATH}
-  fi
-  if [ ! -d ${REPO_PATH}/.git ] ; then
-    cd ${REPO_PATH}
-    git clone git://git:/data/repo/ .
-    su devel -c 'git config --global user.email "devel-devel@mail.arx"'
-    su devel -c 'git config --global user.name "devel"'
-    chown -R ${USER}:${USER} ${REPO_PATH}
-    /bin/bash
-  else
-    cd ${REPO_PATH}
-    git pull
-  fi
-else
-  echo "Running as root!"
+if [ ! -d ${REPO_PATH} ] ; then
+  mkdir -p ${REPO_PATH}
+fi
+cd ${REPO_PATH}
+if [ ! -d ${REPO_PATH}/.git ] ; then
+  git clone git://git:/data/repo/ .
+  su devel -c 'git config --global user.email "devel-devel@mail.arx"'
+  su devel -c 'git config --global user.name "${USER}"'
+  chown -R ${USER}:${USER} ${REPO_PATH}
   /bin/bash
 fi
+git pull
 
-#tail -f /dev/null
 while true; do sleep 30; done;
