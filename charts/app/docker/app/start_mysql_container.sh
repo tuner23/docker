ea7bee3f91c8ebf6346a7f309378e6d332a23b6c
tuner23 <antonios.dimtsoudis@gmx.de>
2020-03-01 17:00:44 +0000

    fixes on app-mysql chart

--- charts/app/docker/app/start_mysql_container.sh
+++ charts/app/docker/app/start_mysql_container.sh
@@ -8,20 +8,13 @@
 # Setup database
 if [ ! -d "${REPO_PATH}" ]; then
   cp -pr /var/lib/mysql /data/
-  mkdir -p /data/tmp.load
-  chmod 700 /data/tmp.load
   chown -R ${USER}:mysql /data
 
   /usr/sbin/mysqld --defaults-file=/etc/mysql/my.cnf --secure-file-priv=/data/tmp.load --explicit_defaults_for_timestamp --basedir=/usr --log-bin=mysqld-bin &
   sleep 10
-    # Kill the anonymous users
-    mysql -e "DROP USER ''@'localhost'"
     # Because our hostname varies we'll use some Bash magic here.
-    mysql -e "DROP USER ''@'$(hostname)'"
     mysql -e "CREATE USER 'projects'@'%' IDENTIFIED BY 'lpBiG423';"
     mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'projects'@'%';"
-    # Kill off the demo database
-    mysql -e "DROP DATABASE test"
     
     # Make our changes take effect
     mysql -e "FLUSH PRIVILEGES"
