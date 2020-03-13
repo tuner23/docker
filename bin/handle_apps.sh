837629afd75b04067d083b975e70bef64c92bcb2
tuner23 <antonios.dimtsoudis@gmx.de>
2020-03-13 18:07:40 +0000

    improovements on app chart

--- bin/handle_apps.sh
+++ bin/handle_apps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+kubectl='microk8s.kubectl'
 #WORKDIR="${SCRIPTPATH}/../charts"
 #CMDS="install upgrade delete purge"
 #FORCE=""
@@ -9,8 +10,9 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 usage() { 
     echo -e "Usage:\n$0 [service|login]"
     echo -e "Options:"
-    echo -e "\tservice [SERVICENAME]: \tGet service url"
-    echo -e "\tlogin [SERVICENAME]: \tPrint docker login command"
+    echo -e "\tservices [SERVICENAME]: \tGet services"
+    echo -e "\tpods [SERVICENAME]: \tGet pods"
+    echo -e "\tlogin [SERVICENAME]: \tlogin to container"
 }
 
 if [ "$#" -lt 1 ] ; then
@@ -32,17 +34,39 @@ error_exit() {
 	exit 1
 }
 
-get_service_url() {
-    minikube --namespace=development service $OPT --url
+get_services() {
+    echo "NAME         TYPE       CLUSTER-IP       EXTERNAL-IP   PORT(S)          AGE"
+    echo "-- mg -------"
+    $kubectl get services --namespace=mg | grep -v "CLUSTER-IP"
+    echo "-- devel ----"
+    $kubectl get services --namespace=development | grep -v "CLUSTER-IP"
 }
 
+get_pods() {
+    echo "NAME           READY   STATUS    RESTARTS   AGE"
+    echo "-- mg -------"
+    $kubectl get pods --namespace=mg | grep -v "READY"
+    echo "-- devel ----"
+    $kubectl get pods --namespace=development | grep -v "READY"
+}
+
+
 login_container() {
-    
+    echo ${OPT}
+    if [[ "${OPT}" =~ ^app-.* ]] ; then
+        NS="mg"
+    else
+        NS="development"
+    fi
+    $kubectl exec ${OPT}-0 --namespace=${NS}  -it -- /bin/bash
 }
 
 case "${CMD}" in
-    service)
-        get_service_url $OPT
+    services)
+        get_services $OPT
+        ;;
+    pods)
+        get_pods $OPT
         ;;
     login)
         login_container $OPT
